f553df1efb93996215eb55a4ed5359d37ebfbd06
mollikacomputer <mollikacomputer3@gmail.com>
2023-02-20 00:30:50 +0600

    getProduct added

--- src/app/store/store.js
+++ src/app/store/store.js
@@ -1,12 +1,15 @@
 import {configureStore} from "@reduxjs/toolkit";
 import cartSlice from "../../features/card/cartSlice";
 import filterSlice from "../../features/filter/filterSlice";
-
+import logger from "redux-logger";
+import productsSlice from "../../features/products/productsSlice";
 const store = configureStore({
     reducer:{
         cart:cartSlice,
         filter:filterSlice,
-    }
+        products: productsSlice,
+    },
+    middleware:(getDefaultMiddleWare)=> getDefaultMiddleWare().concat(logger)
 });
 
 export default store;
