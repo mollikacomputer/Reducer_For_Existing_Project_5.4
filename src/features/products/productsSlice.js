5a631cb1bab90a25de5edd7c7f890c2348de5372
mollikacomputer <mollikacomputer3@gmail.com>
2023-02-25 20:27:12 +0600

    env local file added

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,4 +1,5 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { fetchProducts } from "./productsApi";
 
 const initialState = {
     products:[],
@@ -7,9 +8,8 @@ const initialState = {
     error:"",
 };
 export const getProducts = createAsyncThunk("products/getProduct", async ()=>{
-    const res = await fetch("http://localhost:5000/products");
-    const data = await res.json();
-    return data.data;
+    const products = fetchProducts();
+    return products;
 });
 
 const productsSlice = createSlice({
