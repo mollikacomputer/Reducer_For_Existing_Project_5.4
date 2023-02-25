89b1a9b7e6fc82b566abe9b227d154f4ebd2d530
mollikacomputer <mollikacomputer3@gmail.com>
2023-02-25 21:58:31 +0600

    some added

--- src/features/products/productsSlice.js
+++ src/features/products/productsSlice.js
@@ -1,8 +1,9 @@
 import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
-import { fetchProducts } from "./productsApi";
+import { fetchProducts, postProduct } from "./productsApi";
 
 const initialState = {
     products:[],
+    postSuccess:false,
     isLoading:false,
     isError:false,
     error:"",
@@ -11,13 +12,17 @@ export const getProducts = createAsyncThunk("products/getProduct", async ()=>{
     const products = fetchProducts();
     return products;
 });
+export const addProduct = createAsyncThunk("products/addProduct", async (data)=>{
+    const products = postProduct(data);
+    return products;
+});
 
 const productsSlice = createSlice({
     name:"products",
     initialState,
     extraReducers:(builder)=>{
         builder
-        .addCase(getProducts.pending, (state, action)=>{
+        .addCase(getProducts.pending, (state)=>{
             state.isLoading = true;
             state.isError = false;
         })
@@ -29,6 +34,25 @@ const productsSlice = createSlice({
         .addCase(getProducts.rejected, (state, action)=>{
             state.products = [];
             state.isLoading = false;
+            state.isError = true;
+            state.error = action.error.message;
+
+        })
+
+        .addCase(addProduct.pending, (state)=>{
+            state.isLoading = true;
+            state.postSuccess = false;
+            state.isError = false;
+        })
+        .addCase(addProduct.fulfilled, (state)=>{
+            state.postSuccess = true;
+            state.isLoading = false;
+
+        })
+        .addCase(addProduct.rejected, (state, action)=>{
+            state.products = [];
+            state.isLoading = false;
+            state.postSuccess = false;
             state.isError = true;
             state.error = action.error.message;
 
